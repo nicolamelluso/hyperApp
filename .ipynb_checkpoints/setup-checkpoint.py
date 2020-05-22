3343013c22fd6a41d1b8de45c8ed90b1ff89483e
nicolamelluso <your_email@example.com>
2020-05-22 16:43:52 +0200

    pandas

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(name='alone',
       install_requires=[
-        'numpy', 'cython','graphbrain'   ],
+        'pandas', 'cython','graphbrain'   ],
       url = 'https://github.com/nicolamelluso/graphbrain', download_url = 'https://github.com/nicolamelluso/graphbrain', dependency_links = ["https://github.com/graphbrain/graphbrain#egg=graphrain"])
 
 
